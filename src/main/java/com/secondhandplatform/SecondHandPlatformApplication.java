6281c487d8e34ab5e4d42f3bf7457353edf01532
Leegeonwoo7 <workgeon07@naver.com>
2024-08-09 01:51:33 +0900

    init project

---
+++ src/main/java/com/secondhandplatform/SecondHandPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.secondhandplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHandPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondHandPlatformApplication.class, args);
+    }
+
+}
