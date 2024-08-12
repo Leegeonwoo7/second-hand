73397ce49f986554829bc323f4fb0fc1b43eb64b
Leegeonwoo7 <workgeon07@naver.com>
2024-08-12 23:35:42 +0900

    feat: 이메일 생성자 구현

---
+++ src/main/java/com/secondhandplatform/api/controller/UserController.java
@@ -0,0 +1,2 @@
+package com.secondhandplatform.api.controller;public class UserController {
+}
---
+++ src/main/java/com/secondhandplatform/provider/EmailProvider.java
@@ -0,0 +1,47 @@
+package com.secondhandplatform.provider;
+
+import com.secondhandplatform.exception.EmailSendException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class EmailProvider {
+
+    private final JavaMailSender javaMailSender;
+
+    private final String SUBJECT = "[Second Hand] 인증 이메일 번호입니다.";
+
+
+    public boolean sendMail(String email, String certificationNumber) {
+        try{
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message, true);
+
+            String htmlContent = getCertificationMessage(certificationNumber);
+
+            messageHelper.setTo(email);
+            messageHelper.setSubject(SUBJECT);
+            messageHelper.setText(htmlContent, true);
+
+            javaMailSender.send(message);
+        } catch (Exception e){
+            log.error("Failed to send email to {}", email, e);
+            throw new EmailSendException("이메일 전송에 실패했습니다.", e);
+        }
+
+        return true;
+    }
+
+    private String getCertificationMessage(String certificationNumber) {
+        String certificationMessage = "";
+        certificationMessage += "<h1 style='text-align: center;'>[인증번호] 인증메일</h1>";
+        certificationMessage += "<h3 style='text-align: center;'>인증코드 : <strong style='font-size: 32px; letter-spacing: 8px;'>" + certificationNumber + "</strong></h3>";
+        return certificationMessage;
+    }
+}
