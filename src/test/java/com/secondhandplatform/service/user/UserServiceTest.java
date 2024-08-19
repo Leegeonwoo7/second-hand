dbb23ebe7c496853c9d8d5f6ed04a4759ab6898b
Leegeonwoo7 <workgeon07@naver.com>
2024-08-19 14:23:39 +0900

    test: 이메일 인증요청 검증 테스트 구현

--- src/test/java/com/secondhandplatform/service/user/UserServiceTest.java
+++ src/test/java/com/secondhandplatform/service/user/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.secondhandplatform.service.user;
 
+import com.secondhandplatform.api.request.user.CheckCertificationRequest;
 import com.secondhandplatform.api.request.user.DuplicateLoginIdRequest;
 import com.secondhandplatform.api.request.user.SendCertificationRequest;
 import com.secondhandplatform.domain.user.Certification;
@@ -7,6 +8,7 @@ import com.secondhandplatform.domain.user.SignupType;
 import com.secondhandplatform.domain.user.User;
 import com.secondhandplatform.domain.user.UserType;
 import com.secondhandplatform.repository.CertificationRepository;
+import com.secondhandplatform.service.user.response.CheckCertificationResponse;
 import com.secondhandplatform.service.user.response.DuplicateLoginIdResponse;
 import com.secondhandplatform.service.user.response.SendCertificationResponse;
 import org.junit.jupiter.api.*;
@@ -15,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
@@ -28,7 +31,6 @@ class UserServiceTest {
 
     @Autowired
     private UserRepository userRepository;
-
     @Autowired
     private CertificationRepository certificationRepository;
 
@@ -99,6 +101,95 @@ class UserServiceTest {
         //then
         assertThat(response.isSuccess()).isTrue();
         assertThat(findCertification.getEmail()).isEqualTo(email);
+    }
+
+//    SendCertificationRequest sendRequest = SendCertificationRequest.builder()
+//            .email(email)
+//            .build();
+//        userService.sendCertification(sendRequest);
+    @Test
+    @Order(4)
+    @DisplayName("이메일 인증번호 요청확인을 성공한다")
+    void checkCertification_success() {
+        // given
+        String email = "test@example.com";
+        String certificationNumber = "1234";
+        Certification certification = Certification.create(email, certificationNumber);
+        certificationRepository.save(certification);
+
+        CheckCertificationRequest request = new CheckCertificationRequest(email, certificationNumber);
+
+        //when
+        CheckCertificationResponse response = userService.checkCertification(request);
+
+        //then
+        assertThat(response).isNotNull();
+        assertThat(response.isSuccess()).isTrue();
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("이메일 인증번호 요청확인시 잘못된 이메일로 요청한다")
+    void checkCertification_fail_email() {
+        // given
+        String email = "test@example.com";
+        String certificationNumber = "1234";
+        Certification certification = Certification.create(email, certificationNumber);
+        certificationRepository.save(certification);
+
+        CheckCertificationRequest request = new CheckCertificationRequest("newEmail@example.com", certificationNumber);
+
+        //when
+        CheckCertificationResponse response = userService.checkCertification(request);
+
+        //then
+        assertThat(response).isNotNull();
+        assertThat(response.isSuccess()).isFalse();
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("이메일 인증번호 요청확인시 잘못된 인증번호로 요청한다")
+    void checkCertification_fail_number() {
+        // given
+        String email = "test@example.com";
+        String certificationNumber = "1234";
+        Certification certification = Certification.create(email, certificationNumber);
+        certificationRepository.save(certification);
+
+        CheckCertificationRequest request = new CheckCertificationRequest(email, "6789");
+
+        //when
+        CheckCertificationResponse response = userService.checkCertification(request);
+
+        //then
+        assertThat(response).isNotNull();
+        assertThat(response.isSuccess()).isFalse();
+    }
+
+    @Test
+    @Order(7)
+    @DisplayName("이메일 인증번호 요청확인시 만료된 인증번호로 요청한다")
+    void checkCertification_fail_expireTime() {
+        // given
+        String email = "test@example.com";
+        String certificationNumber = "1234";
+        Certification certification = Certification.builder()
+                .email(email)
+                .certificationNumber(certificationNumber)
+                .expiresAt(LocalDateTime.now().minusMinutes(30))
+                .build();
+
+        certificationRepository.save(certification);
+
+        CheckCertificationRequest request = new CheckCertificationRequest(email, certificationNumber);
+
+        //when
+        CheckCertificationResponse response = userService.checkCertification(request);
+
+        //then
+        assertThat(response).isNotNull();
+        assertThat(response.isSuccess()).isFalse();
     }
 
     private static User createUser(
