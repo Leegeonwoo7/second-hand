30add44345717ccc19524e5fdfc9c707b09cd741
Leegeonwoo7 <workgeon07@naver.com>
2024-08-27 16:44:41 +0900

    feat: [Repository] 회원가입 중복 테스트

--- src/test/java/com/secondhandplatform/repository/UserRepositoryTest.java
+++ src/test/java/com/secondhandplatform/repository/UserRepositoryTest.java
@@ -1,10 +1,16 @@
 package com.secondhandplatform.repository;
 
+import com.secondhandplatform.domain.user.SignupType;
+import com.secondhandplatform.domain.user.User;
+import com.secondhandplatform.domain.user.UserType;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
@@ -15,12 +21,15 @@ class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
-
+    @AfterEach
+    void tearDown() {
+        userRepository.deleteAllInBatch();
+    }
 
     @Test
     @Order(1)
     @DisplayName("존재하지 않는 아이디")
-    void asd() {
+    void nonExistId() {
         // given
         String nonExistId = "nonExistId";
 
@@ -29,5 +38,35 @@ class UserRepositoryTest {
 
         //then
         assertThat(result).isFalse();
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("이미 존재하는 아이디")
+    void existId() {
+        // given
+        String existId = "existId";
+        User user = createUser(existId);
+        userRepository.save(user);
+
+        //when
+        boolean result = userRepository.existsByLoginId(existId);
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    private User createUser(String existId) {
+        LocalDate birthday = LocalDate.of(1992, 2, 15);
+
+        return User.builder()
+                .loginId(existId)
+                .password("1234")
+                .email("email")
+                .phone("01012341234")
+                .birthday(birthday)
+                .userType(UserType.USER)
+                .signupType(SignupType.APP)
+                .build();
     }
 }
