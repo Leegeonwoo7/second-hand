e8466eefdeac8fbe4d2fb5447f379c8871fcec68
Leegeonwoo7 <workgeon07@naver.com>
2024-08-30 17:15:41 +0900

    feat: 회원가입 응답 DTO

--- src/main/java/com/secondhandplatform/dto/user/response/CreateUserResponseDto.java
+++ src/main/java/com/secondhandplatform/dto/user/response/CreateUserResponseDto.java
@@ -1,2 +1,19 @@
-package com.secondhandplatform.dto.user.response;public class CreateUserResponseDto {
+package com.secondhandplatform.dto.user.response;
+
+import com.secondhandplatform.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateUserResponseDto {
+
+    private Long id;
+    private String message;
+
+
 }
