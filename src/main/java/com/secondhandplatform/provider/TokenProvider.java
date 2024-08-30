a7b78260147b7db1531e3fc32f0ceda89f71021c
Leegeonwoo7 <workgeon07@naver.com>
2024-08-30 13:03:44 +0900

    feat: JWT토큰 생성 구현

---
+++ src/main/java/com/secondhandplatform/provider/TokenProvider.java
@@ -0,0 +1,55 @@
+package com.secondhandplatform.provider;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TokenProvider {
+
+    @Value("${secret-key}")
+    private String secretKey;
+
+    public String create(String loginId) {
+        Date expiredDate = new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24);
+
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.builder()
+                .signWith(key, SignatureAlgorithm.HS256)
+                .setSubject(loginId)
+                .setIssuedAt(new Date())
+                .setExpiration(expiredDate)
+                .compact();
+    }
+
+    public String validate(String token) {
+        String subject = null;
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+        try {
+            subject = Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getSubject();
+        } catch (Exception e) {
+            log.warn("JWT 토큰 검증 실패");
+            e.printStackTrace();
+            return null;
+        }
+
+        return subject;
+    }
+}
