e30c967a6861a6baa639659a72efbcb8c34de04e
Leegeonwoo7 <workgeon07@naver.com>
2024-08-30 17:14:46 +0900

    feat: 스프링 시큐리티 설정파일 / 커스텀 필터 구현

--- src/main/java/com/secondhandplatform/config/AuthenticationFilter.java
+++ src/main/java/com/secondhandplatform/config/AuthenticationFilter.java
@@ -1,2 +1,92 @@
-package com.secondhandplatform.config;public class AuthenticationFilter {
+package com.secondhandplatform.config;
+
+import com.secondhandplatform.domain.user.User;
+import com.secondhandplatform.domain.user.UserType;
+import com.secondhandplatform.provider.TokenProvider;
+import com.secondhandplatform.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AuthenticationFilter extends OncePerRequestFilter {
+
+    private final TokenProvider tokenProvider;
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.debug("Start my filter...");
+
+        try {
+            String token = parseBearerToken(request);
+            if (token == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            String loginId = tokenProvider.validate(token);
+            if (loginId == null) {
+                log.debug("tokenProvider.validate: {}", loginId);
+                filterChain.doFilter(request, response);
+            }
+
+            User user = userRepository.findByLoginId(loginId);
+            String role = user.getUserType()
+                    .toString();
+            log.debug("user.getRole.toString: {}", role);
+
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority(role));
+
+            SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginId, null, authorities);
+
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            securityContext.setAuthentication(authenticationToken);
+            SecurityContextHolder.setContext(securityContext);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        filterChain.doFilter(request, response);
+    }
+
+
+
+    private String parseBearerToken(HttpServletRequest request) {
+        String authorization = request.getHeader("Authorization");
+
+        boolean hasAuthorization = StringUtils.hasText(authorization);
+        if (!hasAuthorization) {
+            log.warn("요청에 토큰이 존재하지 않습니다.");
+            return null;
+        }
+
+        boolean isBearer = authorization.startsWith("Bearer ");
+        if (!isBearer) {
+            log.warn("토큰이 존재하지만 Bearer토큰이 아닙니다.");
+            return null;
+        }
+
+        return authorization.substring(7);
+    }
 }
--- src/main/java/com/secondhandplatform/config/SecurityConfig.java
+++ src/main/java/com/secondhandplatform/config/SecurityConfig.java
@@ -1,42 +1,41 @@
-//package com.secondhandplatform.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//
-//        // 접근 권한 URL
-//        http.authorizeHttpRequests((auth) -> auth
-//                        .requestMatchers("/").permitAll()
-//                .requestMatchers("/", "/users/login", "/users/sign-up").permitAll()
-//                .requestMatchers("/users/**").hasAnyRole("ADMIN", "USER")
-//                .requestMatchers("/admin/**").hasRole("ADMIN")
-//                .anyRequest().authenticated()
-//        );
-//
-//        // 로그인 폼
-//        http.formLogin((auth) -> auth
-//                .loginPage("/users/login").permitAll()
-//                .failureUrl("/users/login-error")
-//        );
-//
-//        // TODO CSRF
-//        http.csrf((auth) -> auth.disable());
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//}
+package com.secondhandplatform.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final AuthenticationFilter authenticationFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/", "/users/**").permitAll()
+        );
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
