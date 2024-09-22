63ade6e26d0ecb42f9faa73d17e3b58a9f679100
Leegeonwoo7 <workgeon07@naver.com>
2024-09-22 22:08:34 +0900

    feat: 서비스계층 주문 생성 구현

--- src/main/java/com/secondhandplatform/order/dto/response/OrderResponse.java
+++ src/main/java/com/secondhandplatform/order/dto/response/OrderResponse.java
@@ -1,13 +1,16 @@
-package com.secondhandplatform.order.dto.request;
+package com.secondhandplatform.order.dto.response;
 
 import com.secondhandplatform.delivery.domain.Delivery;
+import com.secondhandplatform.order.domain.Order;
 import com.secondhandplatform.order.domain.OrderStatus;
 import com.secondhandplatform.payment.domain.Payment;
 import com.secondhandplatform.product.domain.Product;
 import com.secondhandplatform.user.domain.User;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class OrderResponse {
 
     private Long id;
@@ -19,4 +22,18 @@ public class OrderResponse {
     private User buyer;
     private User seller;
     private Product product;
+
+    public static OrderResponse of(Order order) {
+        return OrderResponse.builder()
+                .id(order.getId())
+                .orderStatus(order.getOrderStatus())
+                .totalPrice(order.getTotalPrice())
+                .quantity(order.getQuantity())
+                .payment(order.getPayment())
+                .delivery(order.getDelivery())
+                .buyer(order.getBuyer())
+                .seller(order.getSeller())
+                .product(order.getProduct())
+                .build();¥
+    }
 }
