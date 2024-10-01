0af27c2fc56b615c5d3957efc1f9160688377c81
Leegeonwoo7 <workgeon07@naver.com>
2024-10-02 03:12:45 +0900

    feat: <React.StrictMode>사용으로 useEffect가 두 번씩 호출되기때문에 개발중 주석처리

--- second-hand-front/src/index.js
+++ second-hand-front/src/index.js
@@ -6,9 +6,10 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+    // 개발모드에서 React.StrcitMode를 사용할 경우 버그의 발생을 방지하기위해 useEffect를 두번씩 호출함 프로덕션에서는 한번씩 호출되기때문에 배포시에는 주석을 해제해주어야함.
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 reportWebVitals();
